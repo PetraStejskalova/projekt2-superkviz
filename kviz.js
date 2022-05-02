39745131b9cb5adc89d71638f8d2142ed394aa89
Petra Stejskalova <pt.stejskalova@gmail.com>
2022-05-02 15:49:37 +0200

    correction of questions display  > undefined in rating removed

--- kviz.js
+++ kviz.js
@@ -29,103 +29,59 @@ const spravneOdpovedi = ['Mončičák', 'Jahoda', 'Umět JavaScript'];
 let poradi = document.getElementById('poradi');
 let otazka = document.getElementById('otazka');
 let obrazek = document.getElementById('obrazek');
-let odpovedi = document.createElement('ul');
-let prvniMoznost = document.createElement('li');
-let druhaMoznost = document.createElement('li');
-let tretiMoznost = document.createElement('li');
-let ctvrtaMoznost = document.createElement('li');
-let i = 0;
+let moznosti = document.querySelector('#moznosti');
+
 let kviz = document.querySelector('.kviz');
 let hodnoceni = document.getElementById('hodnoceni');
 let vysledek = document.querySelector('.vysledek');
-let a = 0;
-
-odpovedi.setAttribute('id', 'odpovedi');
-
-prvniMoznost.dataset.odpoved = 0;
-druhaMoznost.dataset.odpoved = 1;
-tretiMoznost.dataset.odpoved = 2;
-ctvrtaMoznost.dataset.odpoved = 3;
 
 let tvojeOdpovedi = [];
-console.log(tvojeOdpovedi);
+
+let aktualniOtazka = 0;
 
 let spravne = 0;
 
+zobrazOtazku();
+
 // Tato funkce se postará o vygenerování otázky
 // Zavoláme ji jednou na začátku a poté vždy po odpovězení
-
 function zobrazOtazku() {
+    poradi.textContent = 'Otázka ' + (aktualniOtazka + 1) + ' / ' + otazky.length;
+    otazka.textContent = otazky[aktualniOtazka].otazka;
+    obrazek.src = otazky[aktualniOtazka].foto;
 
-    poradi.textContent = otazky[i].poradi;
-    otazka.textContent = otazky[i].otazka;
-    obrazek.src = otazky[i].foto;
+    let odpovedi = otazky[aktualniOtazka].odpovedi;
 
-    prvniMoznost.textContent = otazky[i].odpovedi[0];
-    druhaMoznost.textContent = otazky[i].odpovedi[1];
-    tretiMoznost.textContent = otazky[i].odpovedi[2];
-    ctvrtaMoznost.textContent = otazky[i].odpovedi[3];
+    let seznam = document.createElement('ul');
+    seznam.setAttribute('id', 'odpovedi');
 
-    prvniMoznost.onclick = klikNaOdpoved;
-    druhaMoznost.onclick = klikNaOdpoved;
-    tretiMoznost.onclick = klikNaOdpoved;
-    ctvrtaMoznost.onclick = klikNaOdpoved;
-
-    console.log(i);
-    console.log(otazky[i].odpovedi.length);
-
-    if (otazky[i].odpovedi.length === 4) {
-        odpovedi.appendChild(prvniMoznost);
-        odpovedi.appendChild(druhaMoznost);
-        odpovedi.appendChild(tretiMoznost);
-        odpovedi.appendChild(ctvrtaMoznost);
-    }
-    if (otazky[i].odpovedi.length === 3) {
-        odpovedi.appendChild(prvniMoznost);
-        odpovedi.appendChild(druhaMoznost);
-        odpovedi.appendChild(tretiMoznost);
-    }
-    if (otazky[i].odpovedi.length < 3) {
-        odpovedi.appendChild(prvniMoznost);
-        odpovedi.appendChild(druhaMoznost);
-        odpovedi.removeChild(tretiMoznost);
-        odpovedi.removeChild(ctvrtaMoznost);
+    for (let i = 0; i < odpovedi.length; i++) {
+        let polozka = document.createElement('li');
+        polozka.dataset.odpoved = i;
+        polozka.textContent = odpovedi[i];
+        polozka.onclick = klikNaOdpoved;
+        seznam.appendChild(polozka);
     }
 
-    let mozneOdpovedi = document.querySelectorAll('li');
-
-    mozneOdpovedi.forEach((moznost) => {
-        moznost.addEventListener('click', pridejDoPole);
-    });
-
-    function pridejDoPole(udalost) {
-        let moznost = udalost.target;
-
-        let indexOdpovedi = moznost.dataset.odpoved;
-
-        tvojeOdpovedi.push(indexOdpovedi);
-
-        console.log(tvojeOdpovedi);
-    }
-}
-
-function onLoad() {
-    zobrazOtazku();
+    moznosti.appendChild(seznam);
 }
 
 // Funkce se postará o obsluhu kliknutí na odpověď
 // Musíme ji navázat na kokrétní odpovědi každé otázky (to uděláme v rámci funkce zobrazOtazku())
 function klikNaOdpoved() {
-    if (i < otazky.length - 1) {
-        i++
+    let odpoved = event.target.dataset.odpoved;
+
+    tvojeOdpovedi.push(odpoved);
+
+    aktualniOtazka = aktualniOtazka + 1;
+
+    if (aktualniOtazka < otazky.length) {
+        document.querySelector('#odpovedi').remove();
         zobrazOtazku();
     } else {
         zobrazVyhodnoceni();
     }
 }
-
-let moznosti = document.querySelector('#moznosti');
-moznosti.appendChild(odpovedi);
 
 // Když už mám odpovězeno na vše (řídí se velikosí objektu otazky na řádku 3), tak mohu zobrazi výsledky
 // Vypočítám skóre a nageneruje nové elementy do HTML
@@ -134,26 +90,26 @@ function zobrazVyhodnoceni() {
     kviz.style.display = 'none';
     vysledek.style.display = 'block';
 
-    for (let a = 0; a < otazky.length; a++) {
+    for (let i = 0; i < otazky.length; i++) {
         const hodnoceniOtazky = document.createElement('div');
         hodnoceniOtazky.classList.add('hodnoceni-otazky');
 
         const shrnutiOtazky = document.createElement('h3');
         shrnutiOtazky.classList.add('shrnuti-otazky');
-        shrnutiOtazky.textContent = otazky[a].index + '. ' + otazky[a].otazka;
+        shrnutiOtazky.textContent = otazky[i].index + '. ' + otazky[i].otazka;
 
         const tvojeOdpoved = document.createElement('p');
         tvojeOdpoved.classList.add('tvoje-odpoved');
-        tvojeOdpoved.textContent = 'Tvoje odpověď: ' + otazky[a].odpovedi[tvojeOdpovedi[a]];
+        tvojeOdpoved.textContent = 'Tvoje odpověď: ' + otazky[i].odpovedi[tvojeOdpovedi[i]];
 
         const vyhodnoceniOtazky = document.createElement('p');
         vyhodnoceniOtazky.classList.add('vyhodnoceni-otazky');
 
-        if (otazky[a].odpovedi[tvojeOdpovedi[a]] === spravneOdpovedi[a]) {
+        if (otazky[i].odpovedi[tvojeOdpovedi[i]] === spravneOdpovedi[i]) {
             vyhodnoceniOtazky.textContent = 'To je SPRÁVNĚ';
             spravne++;
         } else {
-            vyhodnoceniOtazky.textContent = 'Správná odpověď: ' + spravneOdpovedi[a];
+            vyhodnoceniOtazky.textContent = 'Správná odpověď: ' + spravneOdpovedi[i];
         }
 
         hodnoceniOtazky.appendChild(shrnutiOtazky);
@@ -162,7 +118,6 @@ function zobrazVyhodnoceni() {
 
         hodnoceni.appendChild(hodnoceniOtazky);
     }
-
     zaverecneHodnoceni();
 }
 
